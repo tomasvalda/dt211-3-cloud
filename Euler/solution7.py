f4615bea7f4c7b37fcf996de322e0642a589c6a3
tomas valda <tomas.valda@gmail.com>
2014-10-02 14:58:52 +0000

    solution 7 no error but still in progress

--- Euler/solution7.py
+++ Euler/solution7.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 def is_prime(num):		# function for check if the number is prime, returns true or false
-	if num == 2
+	if num == 2:
 		return true
 	if num < 2:
 		return false
 	if not num & 1:
 		return false
-	for x in range		# number is prime when we can divide it only by the exact number or 1
+#	for x in range		# number is prime when we can divide it only by the exact number or 1
 		#################
 	return true
 
@@ -15,12 +15,12 @@ def main():
 	isprime = true		# prime check
 	number = 1
 	prime_count = 1		# primes count
-		while isprime = true:
-			if is_prime(number):
-				prime_count = prime_count + 1
-			if prime_count == 10001:	# 10001st prime number
-				print number
-				isprime = false
+	while isprime == true:
+		if is_prime(number):
+			prime_count = prime_count + 1
+		if prime_count == 10001:	# 10001st prime number
+			print number
+			isprime = false
  			number = number + 2		# next prime number
 
 
