e6afcca7a392c755961dda760de32d8f24701fa1
tomas valda <tomas.valda@gmail.com>
2014-10-02 14:13:30 +0000

    second version

--- Euler/solution6.py
+++ Euler/solution6.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 
 sum_square = 0		# sum of squares
-for x in range (1,100):
-	
+sum_num = 0		# sum of numbers
+y = 1			# counter
+difference = 0
+
+for x in range(100):
+	sum_square = sum_square + y * y		# sum of the squares of the first 100 numbers
+	sum_num = sum_num + y			# sum of the first 100 numbers
+	y = y + 1				# counter
+
+sum_num = sum_num * sum_num			# the square of the first 100 numbers
+
+difference = sum_num - sum_square
+
+print difference				# print the difference between
